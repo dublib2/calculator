20961010c9f48c68d6ab1e89e08c4f830d783128
dublib2 <christophe@cdubois.org>
2022-06-28 16:06:46 +0200

    first commit

---
+++ calculator/calculations.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    """Compute and return the sum of two numbers.
+    Examples: 
+    >>> add(4.0, 2.0) 6.0
+    >>> add(4, 2) 6.0
+
+    Args:
+        a (float): A number representing the first addend in the addition.
+        b (float): A number representing the second addend in the addition.
+
+    Returns:
+        float: A number representing the artihmetic sum of `a` and `b`.    """
+
+    return float(a + b)
+
+def subtract(a, b):
+    return float(a - b)
+def multiply(a, b):
+    return float(a * b)
+def divide(a, b):
+  
+    if b == 0:
+        raise ZeroDivisionError("division by zero")
+    return float(a / b)
+
+# print(divide(3,1)) 
+help(add)
+
+
